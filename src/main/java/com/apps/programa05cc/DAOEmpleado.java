80046cc474750ca48b3a33915d1888cceb3ec025
NEAR97 <instantcrushxx97@gmail.com>
2023-04-20 13:57:16 -0600

    first commit

---
+++ src/main/java/com/apps/programa05cc/DAOEmpleado.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.apps.programa05cc;
+
+import java.util.List;
+
+/**
+ *
+ * @author braum
+ */
+public class DAOEmpleado implements IDAOGeneral<Empleado , Long>{
+
+    @Override
+    public Empleado create(Empleado p) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Empleado update(Long id, Empleado p) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Empleado> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Empleado findByID(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/main/java/com/apps/programa05cc/Programa05CC.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.apps.programa05cc;
+
+import org.hibernate.Session;
+
+/**
+ *
+ * @author braum
+ */
+public class Programa05CC {
+
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSession();
+        
+        
+        
+    }
+}
